3ea074695593d65033115dad1f3482ede6364b8e
André Stylianos Ramos <andre.stylianos@protonmail.com>
2023-08-29 16:51:25 +0100

    fix: set auto-pair enable_check_bracket_line=false

--- config/lua/plugins/nvim-autopairs.lua
+++ config/lua/plugins/nvim-autopairs.lua
@@ -3,7 +3,9 @@ local M = {}
 local autopairs = require('nvim-autopairs')
 
 M.config = function()
-    autopairs.setup {}
+    autopairs.setup({
+        enable_check_bracket_line = false
+    })
     autopairs.get_rules("'")[1].not_filetypes = { 'scheme', 'lisp', 'clojure' }
 end
 

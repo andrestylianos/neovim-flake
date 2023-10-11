991d38a31e8ad7e6737756fa2c0e2090207494c2
André Stylianos Ramos <andre.stylianos@protonmail.com>
2023-10-11 14:04:29 +0100

    fix: autopairs default ignored_next_char was too broad

--- config/lua/plugins/nvim-autopairs.lua
+++ config/lua/plugins/nvim-autopairs.lua
@@ -4,7 +4,8 @@ local autopairs = require('nvim-autopairs')
 
 M.config = function()
     autopairs.setup({
-        enable_check_bracket_line = false
+        enable_check_bracket_line = false,
+        ignored_next_char = [=[[%'%"%$]]=]
     })
     autopairs.get_rules("'")[1].not_filetypes = { 'scheme', 'lisp', 'clojure' }
 end
